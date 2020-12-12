0c2767e701d4ebb7e36769fa92c5adc3fd02522c
Seb Jansen <contact@sebjansen.com>
2020-12-12 20:54:18 +0100

    try static urlpatterns

--- base/urls.py
+++ base/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 '''
@@ -34,3 +35,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('message.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
